0e122fb22ae0e522db025ecd744fd4cc50e3bcfd
saudkhan-web <saudkhan2154@gmail.com>
2026-04-09 05:59:49 +0500

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -103,3 +103,4 @@ class DataTransformation:
 
         except Exception as e:
             raise CustomException(e, sys)
+
--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import pickle
 import numpy as np
 import pandas as pd
 import pickle
-
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
@@ -18,6 +18,23 @@ def save_object(file_path, obj):
 
         with open(file_path, 'wb') as file_obj:
             pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+    
+
+def evaluat_models(x_train, y_train, x_test, y_test, models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model.fit(x_train, y_train)  # Train model
+            y_test_pred = model.predict(x_test)  # Predict Testing data
+            train_model_score = r2_score(y_train, model.predict(x_train))  # Evaluate model
+            test_model_score = r2_score(y_test, y_test_pred)  # Evaluate model
+            report[list(models.keys())[i]] = test_model_score  # Save model score in report
 
     except Exception as e:
         raise CustomException(e, sys)
